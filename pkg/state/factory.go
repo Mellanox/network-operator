96d16f916c5d105792f0382a8667134fd0aaa909
Adrian Chiris <adrianc@mellanox.com>
2020-06-14 14:20:26 +0300

    update factory methods

--- pkg/state/factory.go
+++ pkg/state/factory.go
@@ -1,12 +1,39 @@
 package state
 
 import (
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/Mellanox/mellanox-network-operator/pkg/consts"
 )
 
 // NewStateManager creates a state.Manager for the given CRD Kind
 func NewManager(crdKind string, k8sAPIClient client.Client, scheme *runtime.Scheme) (Manager, error) {
-	return &fakeMananger{watchResources: []*source.Kind{}}, nil
+	stateGroups, err := newStates(crdKind, k8sAPIClient, scheme)
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create state manager")
+	}
+
+	if log.V(consts.LogLevelDebug).Enabled() {
+		stateNames := make([][]string, len(stateGroups))
+		for i, sg := range stateGroups {
+			for _, state := range sg.States() {
+				stateNames[i] = append(stateNames[i], state.Name())
+			}
+		}
+		log.V(consts.LogLevelDebug).Info("Creating a new State manager with", "states:", stateNames)
+	}
+
+	return &stateManager{
+		stateGroups: stateGroups,
+		client:      k8sAPIClient,
+	}, nil
+}
+
+// newStates creates States that compose a State manager
+// nolint:unparam
+func newStates(crdKind string, k8sAPIClient client.Client, scheme *runtime.Scheme) ([]Group, error) {
+	return []Group{}, nil
 }
