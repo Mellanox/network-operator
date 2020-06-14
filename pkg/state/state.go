3f0ee4c7ff1ccebd2ec88edcde9e819de98f4b6c
Adrian Chiris <adrianc@mellanox.com>
2020-06-14 13:26:05 +0300

    State API update

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -6,11 +6,13 @@ import (
 
 type SyncState string
 
+// Represents the Sync state of a specific State or a collection of States
 const (
-	SyncStateReady    = "Ready"
-	SyncStateNotReady = "NotReady"
-	SyncStateReset    = "Reset"
-	SyncStateError    = "Error"
+	SyncStateReady    = "ready"
+	SyncStateNotReady = "notReady"
+	SyncStateIgnore   = "ignore"
+	SyncStateReset    = "reset"
+	SyncStateError    = "error"
 )
 
 // State Represents a single State that requires a set of k8s API operations to be performed.
@@ -21,9 +23,12 @@ type State interface {
 	Name() string
 	// Description provides the State description
 	Description() string
-	// Sync attempt to get the system to match the desired state which State represent.
+	// Sync attempt to get the system to match the desired state as depicted in the custom resource
+	// for the bits related to the specific state, State represents.
 	// a sync operation must be relatively short and must not block the execution thread.
-	Sync() (SyncState, error)
+	// ServiceCatalog is provided to optionally provide a State additional services required for it to perform
+	// the Sync operation.
+	Sync(customResource interface{}, serviceCatalog ServiceCatalog) (SyncState, error)
 	// Get a map of source kinds that should be watched for the state keyed by the source kind name
-	GetWatchSources() map[string]source.Kind
+	GetWatchSources() map[string]*source.Kind
 }
