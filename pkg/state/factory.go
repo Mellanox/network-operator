e8e0a802b00f5dac74d6fc183cfd7874cd69922b
Adrian Chiris <adrianc@mellanox.com>
2020-06-14 13:50:09 +0300

    Manager API update

--- pkg/state/factory.go
+++ pkg/state/factory.go
@@ -1,18 +1,13 @@
 package state
 
 import (
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // NewStateManager creates a state.Manager for the given CRD Kind
-func NewManager(crdKind string, k8sAPIClient client.Client) Manager {
-	// TODO: Implement
-	return &fakeMananger{states: newStates(crdKind, k8sAPIClient)}
-}
-
-// newStates creates States that compose a State manager
-// nolint:unparam
-func newStates(crdKind string, k8sAPIClient client.Client) [][]State {
-	// TODO: Implement
-	return [][]State{{&fakeState{client: k8sAPIClient}}}
+func NewManager(crdKind string, k8sAPIClient client.Client, scheme *runtime.Scheme) (Manager, error) {
+	return &fakeMananger{watchResources: []*source.Kind{}}, nil
 }
--- pkg/state/manager.go
+++ pkg/state/manager.go
@@ -1,7 +1,6 @@
 package state
 
 import (
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -9,7 +8,24 @@ import (
 // A state manager invokes states in order to get the system to its desired state
 type Manager interface {
 	// GetWatchSources gets Resources that should be watched by a Controller for this state manager
-	GetWatchSources() []source.Kind
-	// SyncState reconciles the state of the system and updates status for the custom resource
-	SyncState(request *reconcile.Request) (reconcile.Result, error)
+	GetWatchSources() []*source.Kind
+	// SyncState reconciles the state of the system and returns a list of status of the applied states
+	// ServiceCatalog is provided to optionally provide a State additional services required for it to perform
+	// the Sync operation.
+	SyncState(customResource interface{}, serviceCatalog ServiceCatalog) (Results, error)
+}
+
+// Represent a Result of a single State.Sync() invocation
+type Result struct {
+	StateName string
+	Status    SyncState
+	// if SyncStateError then ErrInfo will contain additional error information
+	ErrInfo error
+}
+
+// Represent the Results of a collection of State.Sync() invocations, Status reflects the global status of all states.
+// If all are SyncStateReady then Status is SyncStateReady, if one is SyncStateNotReady, Status is SyncStateNotReady
+type Results struct {
+	Status       SyncState
+	StatesStatus []Result
 }
