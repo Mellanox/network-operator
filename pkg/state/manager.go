0fdd43497ea5db9c524f9a6457336a8b9a8e0c68
Adrian Chiris <adrianc@mellanox.com>
2020-06-14 14:12:16 +0300

    State manager implementation
    This commit provides an implementation of the
    state manager.

--- pkg/state/manager.go
+++ pkg/state/manager.go
@@ -1,7 +1,10 @@
 package state
 
 import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/Mellanox/mellanox-network-operator/pkg/consts"
 )
 
 // StateManager manages a collection of states and handles transitions from State to State.
@@ -28,4 +31,63 @@ type Result struct {
 type Results struct {
 	Status       SyncState
 	StatesStatus []Result
+}
+
+type stateManager struct {
+	stateGroups []Group
+	client      client.Client
+}
+
+func (smgr *stateManager) GetWatchSources() []*source.Kind {
+	kindMap := make(map[string]*source.Kind)
+	for _, stateGroup := range smgr.stateGroups {
+		for _, state := range stateGroup.States() {
+			wr := state.GetWatchSources()
+			// append to kindMap
+			for name, kind := range wr {
+				if _, ok := kindMap[name]; !ok {
+					kindMap[name] = kind
+				}
+			}
+		}
+	}
+
+	kinds := make([]*source.Kind, 0, len(kindMap))
+	kindNames := make([]string, 0, len(kindMap))
+	for kindName, kind := range kindMap {
+		kinds = append(kinds, kind)
+		kindNames = append(kindNames, kindName)
+	}
+	log.V(consts.LogLevelDebug).Info("Watch resources for manager", "sources:", kindNames)
+	return kinds
+}
+
+// SyncState attempts to reconcile the system by invoking Sync on each of the states
+func (smgr *stateManager) SyncState(customResource interface{}, serviceCatalog ServiceCatalog) (Results, error) {
+	// Sync groups of states, transition from one group to the other when a group finishes
+	log.V(consts.LogLevelInfo).Info("Syncing system state")
+	managerResult := Results{
+		Status: SyncStateNotReady,
+	}
+
+	for i, stateGroup := range smgr.stateGroups {
+		log.V(consts.LogLevelInfo).Info("Sync State group", "index", i)
+		results := stateGroup.Sync(customResource, serviceCatalog)
+		done, err := stateGroup.SyncDone()
+		managerResult.StatesStatus = append(managerResult.StatesStatus, results...)
+
+		if err != nil {
+			log.V(consts.LogLevelError).Info("Error while syncing states", "Error:", err)
+			return managerResult, err
+		}
+		if !done {
+			log.V(consts.LogLevelError).Info("State Group Not ready, requeue after 5 seconds")
+			return managerResult, nil
+		}
+		log.V(consts.LogLevelInfo).Info("Sync Completed successfully for State group", "index", i)
+	}
+	// Done Syncing CR
+	managerResult.Status = SyncStateReady
+	log.V(consts.LogLevelInfo).Info("Sync Done for custom resource")
+	return managerResult, nil
 }
