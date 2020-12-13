2dd827a15b4752f1b9541273927dea09e6cd2ea0
Mmduh-483 <lrvt12@hotmail.com>
2020-12-13 13:50:38 +0200

    Update NvGPU node selector to nvidia.com/gpu.present
    Signed-off-by: Mmduh-483 <lrvt12@hotmail.com>

--- pkg/nodeinfo/attributes.go
+++ pkg/nodeinfo/attributes.go
@@ -34,7 +34,7 @@ const (
 	NodeLabelHostname      = "kubernetes.io/hostname"
 	NodeLabelCPUArch       = "kubernetes.io/arch"
 	NodeLabelMlnxNIC       = "feature.node.kubernetes.io/pci-15b3.present"
-	NodeLabelNvGPU         = "feature.node.kubernetes.io/pci-10de.present"
+	NodeLabelNvGPU         = "nvidia.com/gpu.present"
 )
 
 type AttributeType int
