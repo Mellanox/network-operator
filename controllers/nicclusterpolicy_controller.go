a742b829315a1fdad367d8e3ca3641fb30c34a8f
Alexander Maslennikov <amaslennikov@nvidia.com>
2021-10-15 10:35:41 +0300

    Enable psp in makefile deployment
    Signed-off-by: Alexander Maslennikov <amaslennikov@nvidia.com>

--- controllers/nicclusterpolicy_controller.go
+++ controllers/nicclusterpolicy_controller.go
@@ -55,6 +55,7 @@ type NicClusterPolicyReconciler struct {
 // +kubebuilder:rbac:groups=security.openshift.io,resources=securitycontextconstraints,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=security.openshift.io,resourceNames=privileged,resources=securitycontextconstraints,verbs=use
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings;roles;rolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=policy,resources=podsecuritypolicies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=events.k8s.io,resources=events,verbs=create;patch;update
 // +kubebuilder:rbac:groups="",resources=namespaces;serviceaccounts;pods;pods/status;services;services/finalizers;endpoints,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=persistentvolumeclaims;events;configmaps;secrets,verbs=get;list;watch;create;update;patch;delete
