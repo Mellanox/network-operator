b24b0f07857f97c38455cd6f5557bcefcd3bfba1
amaslennikov <amaslennikov@nvidia.com>
2023-05-05 12:44:01 +0300

    Update k8s-operator-libs and other dependencies
    Newer version of k8s-operator-libs contains an important fix for the
    ofed upgrade feature: https://github.com/NVIDIA/k8s-operator-libs/commit/7c8cbe0bd1f390b8bb7912e73b047af584bc597d
    Other dependencies are updated as part of that lib's dependencies.
    
    Also adapted unit tests to work with the latest testing infrastructure.
    
    Signed-off-by: amaslennikov <amaslennikov@nvidia.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -56,9 +55,7 @@ var testEnv *envtest.Environment
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "Controller Suite")
 }
 
 var _ = BeforeSuite(func() {
