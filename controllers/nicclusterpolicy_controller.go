12e4e7a3136060b58288bada51b7a05673e268ee
adrianc <adrianc@nvidia.com>
2023-08-22 18:03:28 +0300

    Remove r/w for openshift scc object
    As we no longer need to create such objects
    (just reference them in role binding) remove these
    permissions from operator cluster role binding
    
    - remove relevant kubebuilder line
    - re-generate kustomize manifests
    - sync deploy and deployment folder with changes to cluster role
    
    Signed-off-by: adrianc <adrianc@nvidia.com>

--- controllers/nicclusterpolicy_controller.go
+++ controllers/nicclusterpolicy_controller.go
@@ -56,7 +56,6 @@ type NicClusterPolicyReconciler struct {
 // In case of adding support for additional types, also update in getSupportedGVKs func in pkg/state/state_skel.go
 //nolint
 // +kubebuilder:rbac:groups=mellanox.com,resources=*,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=security.openshift.io,resources=securitycontextconstraints,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=security.openshift.io,resourceNames=privileged,resources=securitycontextconstraints,verbs=use
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings;roles;rolebindings,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=policy,resources=podsecuritypolicies,verbs=get;list;watch;create;update;patch;delete
