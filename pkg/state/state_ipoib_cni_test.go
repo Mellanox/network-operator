3d684aa095d361221d53dd7fec685aef1775633f
Tobias Giese <tgiese@nvidia.com>
2024-07-29 09:21:36 +0200

    test: adopt new testScope for ipoib cni tests
    Signed-off-by: Tobias Giese <tgiese@nvidia.com>

--- pkg/state/state_ipoib_cni_test.go
+++ pkg/state/state_ipoib_cni_test.go
@@ -19,12 +19,8 @@ package state_test
 import (
 	"context"
 
-	netattdefv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	mellanoxv1alpha1 "github.com/Mellanox/network-operator/api/v1alpha1"
 	"github.com/Mellanox/network-operator/pkg/state"
@@ -32,52 +28,39 @@ import (
 )
 
 var _ = Describe("IPoIB CNI State tests", func() {
-	var ctx context.Context
-	var catalog state.InfoCatalog
-	var client client.Client
-	var cniState state.State
-	var cniRenderer state.ManifestRenderer
+	var ts testScope
 
 	BeforeEach(func() {
-		ctx = context.Background()
-		scheme := runtime.NewScheme()
-		Expect(mellanoxv1alpha1.AddToScheme(scheme)).NotTo(HaveOccurred())
-		Expect(netattdefv1.AddToScheme(scheme)).NotTo(HaveOccurred())
-		client = fake.NewClientBuilder().WithScheme(scheme).Build()
-		manifestDir := "../../manifests/state-ipoib-cni"
-		s, r, err := state.NewStateIPoIBCNI(client, manifestDir)
-		Expect(err).NotTo(HaveOccurred())
-		cniState = s
-		cniRenderer = r
-		catalog = getTestCatalog()
-		catalog.Add(state.InfoTypeStaticConfig,
+		ts = ts.New(state.NewStateIPoIBCNI, "../../manifests/state-ipoib-cni")
+		Expect(ts).NotTo(BeNil())
+
+		ts.catalog.Add(state.InfoTypeStaticConfig,
 			staticconfig.NewProvider(staticconfig.StaticConfig{CniBinDirectory: "custom-cni-bin-directory"}))
 	})
 
 	Context("should render", func() {
 		It("manifests with IPoIB CNI", func() {
 			cr := getNICForIPoIBCNI()
-			objs, err := cniRenderer.GetManifestObjects(context.TODO(), cr, catalog, testLogger)
+			objs, err := ts.renderer.GetManifestObjects(context.TODO(), cr, ts.catalog, testLogger)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(len(objs)).To(Equal(1))
-			GetManifestObjectsTest(ctx, cr, catalog, cr.Spec.SecondaryNetwork.IPoIB, cniRenderer)
+			GetManifestObjectsTest(ts.context, cr, ts.catalog, cr.Spec.SecondaryNetwork.IPoIB, ts.renderer)
 		})
 		It("manifests with IPoIB CNI for Openshift", func() {
 			cr := getNICForIPoIBCNI()
-			openshiftCatalog := getOpenshiftTestCatalog()
-			objs, err := cniRenderer.GetManifestObjects(context.TODO(), cr, openshiftCatalog, testLogger)
+			objs, err := ts.renderer.GetManifestObjects(context.TODO(), cr, ts.openshiftCatalog, testLogger)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(len(objs)).To(Equal(4))
-			GetManifestObjectsTest(ctx, cr, catalog, cr.Spec.SecondaryNetwork.IPoIB, cniRenderer)
+			GetManifestObjectsTest(ts.context, cr, ts.catalog, cr.Spec.SecondaryNetwork.IPoIB, ts.renderer)
 		})
 	})
 
 	Context("should sync", func() {
 		It("without any errors", func() {
 			cr := getNICForIPoIBCNI()
-			err := client.Create(ctx, cr)
+			err := ts.client.Create(ts.context, cr)
 			Expect(err).NotTo(HaveOccurred())
-			status, err := cniState.Sync(ctx, cr, catalog)
+			status, err := ts.state.Sync(ts.context, cr, ts.catalog)
 			Expect(err).NotTo(HaveOccurred())
 			// We do not expect that the sync state (i.e. the DaemonSet) will be ready.
 			// There is no real Kubernetes cluster in the unit tests and thus the Pods cannot be scheduled.
