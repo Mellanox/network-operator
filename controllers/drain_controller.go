9137c3694d1ec0cd1a7befd94e6253e4e4e06405
Ido Heyvi <iheyvi@nvidia.com>
2025-10-05 09:25:11 +0300

    feat: Adding drain controller scuffholds The motiviation is to disable SRIOV OP drain controller, and use Network OP drain controller utilizing maintenance OP.
    Signed-off-by: Ido Heyvi <iheyvi@nvidia.com>

---
+++ controllers/drain_controller.go
@@ -0,0 +1,443 @@
+/*
+2025 NVIDIA CORPORATION & AFFILIATES
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	maintenancev1alpha1 "github.com/Mellanox/maintenance-operator/api/v1alpha1"
+	"github.com/go-logr/logr"
+	sriovnetworkv1 "github.com/k8snetworkplumbingwg/sriov-network-operator/api/v1"
+	constants "github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/consts"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/drain"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/platforms"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/utils"
+	"github.com/k8snetworkplumbingwg/sriov-network-operator/pkg/vars"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/Mellanox/network-operator/pkg/consts"
+	drainer "github.com/Mellanox/network-operator/pkg/drain"
+)
+
+const (
+	// MaxConcurrentDrainReconciles is the maximum number of concurrent reconciles for the drain controller
+	MaxConcurrentDrainReconciles = 50
+)
+
+// DrainReconcile is a struct that contains drain controller configurations
+type DrainReconcile struct {
+	client.Client
+	scheme      *runtime.Scheme
+	recorder    record.EventRecorder
+	drainer     drain.DrainInterface
+	migrationCh chan struct{}
+
+	log logr.Logger
+}
+
+// NewDrainReconcileController creates a new DrainReconcile controller
+func NewDrainReconcileController(client client.Client, scheme *runtime.Scheme, recorder record.EventRecorder,
+	platformHelper platforms.Interface, migrationCh chan struct{}, log logr.Logger) (*DrainReconcile, error) {
+	drainer, err := drainer.NewDrainRequestor(client, log, platformHelper)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DrainReconcile{
+		client,
+		scheme,
+		recorder,
+		drainer,
+		migrationCh,
+		log}, nil
+}
+
+//+kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups=sriovnetwork.openshift.io,resources=sriovnodestates,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=maintenance.nvidia.com,resources=nodemaintenances,verbs=get;list;watch
+//+kubebuilder:rbac:groups=maintenance.nvidia.com,resources=nodemaintenances/status,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+func (r *DrainReconcile) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	select {
+	case <-r.migrationCh:
+	case <-ctx.Done():
+		return ctrl.Result{}, fmt.Errorf("canceled")
+	}
+	reqLogger := r.log.WithName("Drain Reconcile")
+
+	req.Namespace = vars.Namespace
+
+	// get node object
+	node := &corev1.Node{}
+	found, err := r.getObject(ctx, req, node)
+	if err != nil {
+		reqLogger.Error(err, "failed to get node object")
+		return ctrl.Result{}, err
+	}
+	if !found {
+		reqLogger.Info("node not found, don't requeue the request", "node", req.Name)
+		return ctrl.Result{}, nil
+	}
+
+	// get sriovNodeNodeState object
+	nodeNetworkState := &sriovnetworkv1.SriovNetworkNodeState{}
+	found, err = r.getObject(ctx, req, nodeNetworkState)
+	if err != nil {
+		reqLogger.Error(err, "failed to get sriovNetworkNodeState object")
+		return ctrl.Result{}, err
+	}
+	if !found {
+		reqLogger.Info("sriovNetworkNodeState not found, don't requeue the request")
+		return ctrl.Result{}, nil
+	}
+
+	// create the drain state annotation if it doesn't exist in the sriovNetworkNodeState object
+	nodeStateDrainAnnotationCurrent, currentNodeStateExist,
+		err := r.ensureAnnotationExists(ctx, nodeNetworkState, constants.NodeStateDrainAnnotationCurrent)
+	if err != nil {
+		reqLogger.Error(err, "failed to ensure nodeStateDrainAnnotationCurrent")
+		return ctrl.Result{}, err
+	}
+	_, desiredNodeStateExist, err := r.ensureAnnotationExists(ctx, nodeNetworkState,
+		constants.NodeStateDrainAnnotation)
+	if err != nil {
+		reqLogger.Error(err, "failed to ensure nodeStateDrainAnnotation")
+		return ctrl.Result{}, err
+	}
+
+	// create the drain state annotation if it doesn't exist in the node object
+	nodeDrainAnnotation, nodeExist, err := r.ensureAnnotationExists(ctx, node, constants.NodeDrainAnnotation)
+	if err != nil {
+		reqLogger.Error(err, "failed to ensure nodeStateDrainAnnotation")
+		return ctrl.Result{}, err
+	}
+
+	// requeue the request if we needed to add any of the annotations
+	if !nodeExist || !currentNodeStateExist || !desiredNodeStateExist {
+		return ctrl.Result{Requeue: true}, nil
+	}
+	reqLogger.V(consts.LogLevelInfo).Info("Drain annotations", "nodeAnnotation", nodeDrainAnnotation,
+		"nodeStateAnnotation", nodeStateDrainAnnotationCurrent)
+
+	// Check the node request
+	if nodeDrainAnnotation == constants.DrainIdle {
+		// this cover the case the node is on idle
+
+		// node request to be on idle and the current state is idle
+		// we don't do anything
+		if nodeStateDrainAnnotationCurrent == constants.DrainIdle {
+			reqLogger.Info("node and nodeState are on idle nothing todo")
+			return reconcile.Result{}, nil
+		}
+
+		// we have two options here:
+		// 1. node request idle and the current status is drain complete
+		// this means the daemon finish is work, so we need to clean the drain
+		//
+		// 2. the operator is still draining the node but maybe the sriov policy changed and the daemon
+		//  doesn't need to drain anymore, so we can stop the drain
+		if nodeStateDrainAnnotationCurrent == constants.DrainComplete ||
+			nodeStateDrainAnnotationCurrent == constants.Draining {
+			return r.handleNodeIdleNodeStateDrainingOrCompleted(ctx, node, nodeNetworkState)
+		}
+	}
+
+	// this cover the case a node request to drain or reboot
+	if nodeDrainAnnotation == constants.DrainRequired ||
+		nodeDrainAnnotation == constants.RebootRequired {
+		return r.handleNodeDrainOrReboot(ctx, node, nodeNetworkState,
+			nodeDrainAnnotation, nodeStateDrainAnnotationCurrent)
+	}
+
+	reqLogger.Error(nil, "unexpected node drain annotation", "nodeDrainAnnotation", nodeDrainAnnotation)
+	return reconcile.Result{}, fmt.Errorf("unexpected node drain annotation")
+}
+
+func (r *DrainReconcile) getObject(ctx context.Context, req ctrl.Request, object client.Object) (bool, error) {
+	err := r.Get(ctx, req.NamespacedName, object)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (r *DrainReconcile) ensureAnnotationExists(ctx context.Context,
+	object client.Object, key string) (string, bool, error) {
+	value, exist := object.GetAnnotations()[key]
+	if !exist {
+		err := utils.AnnotateObject(ctx, object, key, constants.DrainIdle, r.Client)
+		if err != nil {
+			return "", false, err
+		}
+		return constants.DrainIdle, false, nil
+	}
+
+	return value, true, nil
+}
+
+func (r *DrainReconcile) handleNodeIdleNodeStateDrainingOrCompleted(ctx context.Context,
+	node *corev1.Node,
+	nodeNetworkState *sriovnetworkv1.SriovNetworkNodeState) (ctrl.Result, error) {
+	reqLogger := r.log.WithName("handleNodeIdleNodeStateDrainingOrCompleted")
+	completed, err := r.drainer.CompleteDrainNode(ctx, node)
+	if err != nil {
+		reqLogger.Error(err, "failed to complete drain on node")
+		r.recorder.Event(nodeNetworkState,
+			corev1.EventTypeWarning,
+			"DrainController",
+			"failed to drain node")
+		return ctrl.Result{}, err
+	}
+
+	// if we didn't manage to complete the un drain of the node we retry
+	if !completed {
+		reqLogger.Info("CompleteDrainNode() was not completed re queueing the request")
+		r.recorder.Event(nodeNetworkState,
+			corev1.EventTypeWarning,
+			"DrainController",
+			"CompleteDrainNode() was not completed")
+		// TODO: make this time configurable
+		return reconcile.Result{RequeueAfter: constants.DrainControllerRequeueTime}, nil
+	}
+
+	// check if nodeState annotation is already set to drain idle
+	if utils.ObjectHasAnnotation(nodeNetworkState, constants.NodeStateDrainAnnotationCurrent, constants.DrainIdle) {
+		reqLogger.Info("nodeState annotation is already set to drain idle, nothing to do")
+		return ctrl.Result{}, nil
+	}
+
+	// move the node state back to idle
+	err = utils.AnnotateObject(ctx, nodeNetworkState, constants.NodeStateDrainAnnotationCurrent,
+		constants.DrainIdle, r.Client)
+	if err != nil {
+		reqLogger.Error(err, "failed to annotate node with annotation", "annotation", constants.DrainIdle)
+		return ctrl.Result{}, err
+	}
+
+	reqLogger.Info("completed the un drain for node")
+	r.recorder.Event(nodeNetworkState,
+		corev1.EventTypeNormal,
+		"DrainController",
+		"node un drain completed")
+	return ctrl.Result{}, nil
+}
+
+func (r *DrainReconcile) handleNodeDrainOrReboot(ctx context.Context,
+	node *corev1.Node,
+	nodeNetworkState *sriovnetworkv1.SriovNetworkNodeState,
+	nodeDrainAnnotation,
+	nodeStateDrainAnnotationCurrent string) (ctrl.Result, error) {
+	reqLogger := r.log.WithName("handleNodeDrainOrReboot")
+	// nothing to do here we need to wait for the node to move back to idle
+	if nodeStateDrainAnnotationCurrent == constants.DrainComplete {
+		reqLogger.Info("node requested a drain and nodeState is on drain completed nothing to do")
+		return ctrl.Result{}, nil
+	}
+
+	// Check if we are on a single node, and we require a reboot/full-drain we just return
+	fullNodeDrain := nodeDrainAnnotation == constants.RebootRequired
+	singleNode := false
+	if fullNodeDrain {
+		nodeList := &corev1.NodeList{}
+		err := r.Client.List(ctx, nodeList)
+		if err != nil {
+			reqLogger.Error(err, "failed to list nodes")
+			return ctrl.Result{}, err
+		}
+		if len(nodeList.Items) == 1 {
+			reqLogger.Info("drainNode(): FullNodeDrain requested and we are on Single node")
+			singleNode = true
+		}
+	}
+
+	// call the drain function that will also call drain to other platform providers like openshift
+	drained, err := r.drainer.DrainNode(ctx, node, fullNodeDrain, singleNode)
+	if err != nil {
+		reqLogger.Error(err, "error trying to drain the node")
+		r.recorder.Event(nodeNetworkState,
+			corev1.EventTypeWarning,
+			"DrainController",
+			"failed to drain node")
+		return reconcile.Result{}, err
+	}
+
+	// if we didn't manage to complete the drain of the node we retry
+	if !drained {
+		reqLogger.Info("the nodes was not drained, re queueing the request")
+		r.recorder.Event(nodeNetworkState,
+			corev1.EventTypeWarning,
+			"DrainController",
+			"node drain operation was not completed")
+		return reconcile.Result{RequeueAfter: constants.DrainControllerRequeueTime}, nil
+	}
+
+	// if we manage to drain we label the node state with drain completed and finish
+	err = utils.AnnotateObject(ctx, nodeNetworkState, constants.NodeStateDrainAnnotationCurrent,
+		constants.DrainComplete, r.Client)
+	if err != nil {
+		reqLogger.Error(err, "failed to annotate node with annotation", "annotation", constants.DrainComplete)
+		return ctrl.Result{}, err
+	}
+
+	reqLogger.Info("node drained successfully")
+	r.recorder.Event(nodeNetworkState,
+		corev1.EventTypeNormal,
+		"DrainController",
+		"node drain completed")
+	return ctrl.Result{}, nil
+}
+
+// DrainAnnotationPredicate contains the predicate for node drain annotation changes
+type DrainAnnotationPredicate struct {
+	predicate.Funcs
+	log logr.Logger
+}
+
+//nolint:dupl,revive
+func (DrainAnnotationPredicate) Create(e event.CreateEvent) bool {
+	if e.Object == nil {
+		return false
+	}
+
+	if _, hasAnno := e.Object.GetAnnotations()[constants.NodeDrainAnnotation]; hasAnno {
+		return true
+	}
+	return false
+}
+
+//nolint:dupl,revive
+func (d DrainAnnotationPredicate) Update(e event.UpdateEvent) bool {
+	if e.ObjectOld == nil {
+		return false
+	}
+	if e.ObjectNew == nil {
+		return false
+	}
+
+	oldAnno, hasOldAnno := e.ObjectOld.GetAnnotations()[constants.NodeDrainAnnotation]
+	newAnno, hasNewAnno := e.ObjectNew.GetAnnotations()[constants.NodeDrainAnnotation]
+
+	d.log.V(consts.LogLevelDebug).Info("Update", "oldAnno", oldAnno, "newAnno", newAnno)
+	if !hasOldAnno && hasNewAnno {
+		return true
+	}
+
+	return oldAnno != newAnno
+}
+
+// DrainStateAnnotationPredicate contains the predicate for
+// sriov node state drain annotation changes
+type DrainStateAnnotationPredicate struct {
+	predicate.Funcs
+	log logr.Logger
+}
+
+//nolint:revive
+func (DrainStateAnnotationPredicate) Create(e event.CreateEvent) bool {
+	return e.Object != nil
+}
+
+//nolint:revive
+func (d DrainStateAnnotationPredicate) Update(e event.UpdateEvent) bool {
+	if e.ObjectOld == nil {
+		return false
+	}
+	if e.ObjectNew == nil {
+		return false
+	}
+
+	oldAnno, hasOldAnno := e.ObjectOld.GetAnnotations()[constants.NodeStateDrainAnnotationCurrent]
+	newAnno, hasNewAnno := e.ObjectNew.GetAnnotations()[constants.NodeStateDrainAnnotationCurrent]
+
+	d.log.V(consts.LogLevelDebug).Info("Update", "oldAnno", oldAnno, "newAnno", newAnno)
+	if !hasOldAnno || !hasNewAnno {
+		return true
+	}
+
+	return oldAnno != newAnno
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DrainReconcile) SetupWithManager(mgr ctrl.Manager) error {
+	createUpdateEnqueue := handler.Funcs{
+		CreateFunc: func(_ context.Context, e event.TypedCreateEvent[client.Object],
+			w workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			w.Add(reconcile.Request{NamespacedName: types.NamespacedName{
+				Namespace: drainer.GetDrainRequestorOpts(r.drainer).MaintenanceOPRequestorNS,
+				Name:      e.Object.GetName(),
+			}})
+		},
+		UpdateFunc: func(_ context.Context, e event.TypedUpdateEvent[client.Object],
+			w workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			w.Add(reconcile.Request{NamespacedName: types.NamespacedName{
+				Namespace: drainer.GetDrainRequestorOpts(r.drainer).MaintenanceOPRequestorNS,
+				Name:      e.ObjectNew.GetName(),
+			}})
+		},
+	}
+
+	logger := mgr.GetLogger().WithValues("Function", "Drain")
+	requestorOpts := drainer.GetRequestorOptsFromEnvs()
+	// Watch for spec and annotation changes
+	nodePredicates := builder.WithPredicates(DrainAnnotationPredicate{})
+	nodeStatePredicates := builder.WithPredicates(DrainStateAnnotationPredicate{log: logger})
+	// TODO: Make sure there is once logger instance to be used for all the predicates
+	nodeMaintenancePredicates := drainer.NewConditionChangedPredicate(logger,
+		requestorOpts.MaintenanceOPRequestorID)
+	requestorIDPredicate := drainer.NewRequestorIDPredicate(logger,
+		requestorOpts.MaintenanceOPRequestorID)
+	m := ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: MaxConcurrentDrainReconciles,
+			LogConstructor: func(request *reconcile.Request) logr.Logger {
+				//nolint:lll
+				// Inspired by https://github.com/kubernetes-sigs/controller-runtime/blob/52b17917caa97ec546423867d9637f1787830f3e/pkg/builder/controller.go#L447
+				if req, ok := any(request).(*reconcile.Request); ok && req != nil {
+					logger = logger.WithValues("node", request.Name)
+				}
+				return logger
+			},
+		}).
+		For(&corev1.Node{}, nodePredicates).
+		Watches(&sriovnetworkv1.SriovNetworkNodeState{}, createUpdateEnqueue, nodeStatePredicates).
+		Watches(&maintenancev1alpha1.NodeMaintenance{}, createUpdateEnqueue,
+			builder.WithPredicates(nodeMaintenancePredicates, requestorIDPredicate))
+
+	return m.Complete(r)
+}
--- go.mod
+++ go.mod
@@ -11,9 +11,10 @@ require (
 	github.com/google/go-containerregistry v0.20.6
 	github.com/google/go-containerregistry/pkg/authn/kubernetes v0.0.0-20231129213221-4fdaa32ee934
 	github.com/k8snetworkplumbingwg/network-attachment-definition-client v1.7.7
+	github.com/k8snetworkplumbingwg/sriov-network-operator v1.6.0
 	github.com/onsi/ginkgo/v2 v2.25.3
 	github.com/onsi/gomega v1.38.2
-	github.com/openshift/api v0.0.0-20241015143726-5afd6c371db4
+	github.com/openshift/api v0.0.0-20250227152946-1ee1ef831100
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.11.1
 	github.com/xeipuuv/gojsonschema v1.2.0
@@ -21,7 +22,7 @@ require (
 	k8s.io/api v0.32.9
 	k8s.io/apimachinery v0.32.9
 	k8s.io/client-go v0.32.9
-	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738
+	k8s.io/utils v0.0.0-20241210054802-24370beab758
 	sigs.k8s.io/controller-runtime v0.20.4
 	sigs.k8s.io/yaml v1.6.0
 )
@@ -29,13 +30,33 @@ require (
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
 	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.4.0 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
+	github.com/Mellanox/sriovnet v1.0.3 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/ajeddeloh/go-json v0.0.0-20200220154158-5ae607161559 // indirect
+	github.com/aws/aws-sdk-go v1.55.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cenkalti/hub v1.0.1 // indirect
+	github.com/cenkalti/rpc2 v0.0.0-20210604223624-c1acbc6ec984 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chai2010/gettext-go v1.0.3 // indirect
+	github.com/clarketm/json v1.17.1 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.16.3 // indirect
 	github.com/containers/storage v1.59.1 // indirect
+	github.com/coreos/fcct v0.5.0 // indirect
+	github.com/coreos/go-json v0.0.0-20230131223807-18775e0fb4fb // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/coreos/ign-converter v0.0.0-20230417193809-cee89ea7d8ff // indirect
+	github.com/coreos/ignition v0.35.0 // indirect
+	github.com/coreos/ignition/v2 v2.15.0 // indirect
+	github.com/coreos/vcontext v0.0.0-20230201181013-d72178a18687 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/docker/cli v28.3.2+incompatible // indirect
 	github.com/docker/distribution v2.8.3+incompatible // indirect
@@ -44,33 +65,49 @@ require (
 	github.com/evanphx/json-patch v5.9.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
 	github.com/exponent-io/jsonpath v0.0.0-20210407135951-1de76d718b3f // indirect
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32 // indirect
 	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v1.2.4 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.3 // indirect
-	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
+	github.com/google/gnostic-models v0.6.9 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
+	github.com/google/renameio/v2 v2.0.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jaypipes/ghw v0.13.1-0.20241024164530-c1bfc6e6cd6a // indirect
+	github.com/jaypipes/pcidb v1.0.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/k8snetworkplumbingwg/govdpa v0.1.4 // indirect
+	github.com/k8snetworkplumbingwg/sriov-network-device-plugin v0.0.0-20221127172732-a5a7395122e3 // indirect
+	github.com/k8snetworkplumbingwg/sriovnet v1.2.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/moby/spdystream v0.5.0 // indirect
 	github.com/moby/term v0.5.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -80,27 +117,44 @@ require (
 	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/openshift/client-go v0.0.0-20250125113824-8e1f0b8fa9a7 // indirect
+	github.com/openshift/library-go v0.0.0-20250129210218-fe56c2cf5d70 // indirect
+	github.com/openshift/machine-config-operator v0.0.1-0.20250320230514-53e78f3692ee // indirect
+	github.com/ovn-org/libovsdb v0.6.1-0.20240125124854-03f787b1a892 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.22.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/safchain/ethtool v0.3.0 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/vbatts/tar-split v0.12.1 // indirect
+	github.com/vincent-petithory/dataurl v1.0.0 // indirect
+	github.com/vishvananda/netlink v1.2.1-beta.2.0.20240221172127-ec7bcb248e94 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xlab/treeprint v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.36.0 // indirect
+	go.opentelemetry.io/otel/trace v1.36.0 // indirect
 	go.uber.org/automaxprocs v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	go4.org v0.0.0-20200104003542-c7e774b10ea0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
@@ -113,14 +167,19 @@ require (
 	google.golang.org/protobuf v1.36.7 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	howett.net/plist v1.0.0 // indirect
 	k8s.io/apiextensions-apiserver v0.32.3 // indirect
+	k8s.io/apiserver v0.32.3 // indirect
 	k8s.io/cli-runtime v0.32.3 // indirect
 	k8s.io/component-base v0.32.3 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20241105132330-32ad38e42d3f // indirect
+	k8s.io/kube-aggregator v0.32.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20241212222426-2c72e554b1e7 // indirect
 	k8s.io/kubectl v0.32.3 // indirect
 	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	sigs.k8s.io/kube-storage-version-migrator v0.0.6-0.20230721195810-5c8923c5ff96 // indirect
 	sigs.k8s.io/kustomize/api v0.18.0 // indirect
 	sigs.k8s.io/kustomize/kyaml v0.18.1 // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.4.2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.5.0 // indirect
 )
